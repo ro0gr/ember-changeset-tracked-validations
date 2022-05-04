a27c80760b0fac8d495003d81c9be71c2294c11c
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2022-05-04 14:56:21 +0200

    adjust assertion

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ module.exports = {
     if (this._super.init) { this._super.init.apply(this, arguments); }
 
     var checker = new VersionChecker(this);
-    checker.for('@eflexsystems/ember-tracked-validations', 'npm').assertAbove('5.0.2', 'ember-changeset-tracked-validations requires @eflexsystems/ember-tracked-validations v5.0.0 and above');
+    checker.for('@eflexsystems/ember-tracked-validations', 'npm').assertAbove('5.0.0', 'ember-changeset-tracked-validations requires @eflexsystems/ember-tracked-validations v5.0.0 and above');
   }
 };
